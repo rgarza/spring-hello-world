331a9ccef50c36a0234c24a633852ed0583d27d6
John Jones <JonesJohnM@JohnDeere.com>
2020-10-27 00:43:30 -0500

    adding boilerplate

---
+++ src/test/java/com/jones2026/springhello/SpringHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jones2026.springhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
